2b172c575af8f48732460dac8877f9590c745989
George Aidonidis <george.aidonidis@gmail.com>
2025-06-22 10:45:13 +0300

    Make linter happier

--- src/components/ProgressCircles.stories.tsx
+++ src/components/ProgressCircles.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { ProgressCircles } from './ProgressCircles'
 
 const meta: Meta<typeof ProgressCircles> = {
